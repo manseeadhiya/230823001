3203f0351fac77f552c6e443e91ed9d1ea1877bb
Mansee-Adhiya <adhiyamansee@gmail.com>
2024-03-29 08:35:00 +0530

    Resume Template

---
+++ resume/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Resume from './components/Resume';
+
+
+function App() {
+  return (
+    <>
+   <Resume/>
+   </>
+  );
+}
+
+export default App;
